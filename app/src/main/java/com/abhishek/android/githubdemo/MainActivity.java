7848485e746a7c71f09e7742dec291e632d1edee
abhishek.malaiya <work.abhishekmalaviya@gmail.com>
2020-06-26 18:11:29 +0530

    Initial Commit

--- app/src/main/java/com/abhishek/android/githubdemo/MainActivity.java
+++ app/src/main/java/com/abhishek/android/githubdemo/MainActivity.java
@@ -3,12 +3,18 @@ package com.abhishek.android.githubdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.i(TAG, "onCreate: Commit And Update The App Version in Github");
+
     }
 }
