d5a609b7df46420b6f9d343773b1afc500e916c0
work-abhishek7 <67458638+work-abhishek7@users.noreply.github.com>
2020-06-26 18:29:36 +0530

    Update MainActivity.java

--- app/src/main/java/com/abhishek/android/githubdemo/MainActivity.java
+++ app/src/main/java/com/abhishek/android/githubdemo/MainActivity.java
@@ -13,8 +13,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Log.i(TAG, "onCreate: Commit And Update The App Version in Github");
-
+        
     }
 }
